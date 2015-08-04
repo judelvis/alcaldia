8a448b200e5fafe15e593d88bd68539455228b17
AnthonyBreyner <anthonyuzcategui7@gmail.com>
2015-08-04 17:46:24 -0430

    Gerencias

---
+++ application/views/principal2/gerencias.php
@@ -0,0 +1,307 @@
+<div class="main_bg"><!-- start main -->
+    <div class="container">
+        <div class="main_grid1">
+            <h3 class="style pull-left">Turismo Para Todos</h3>
+
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
+<div class="main1_bg"><!-- start main -->
+    <div class="container">
+        <div class="main_grid">
+            <div class="span_of_4"><!-- start span_of_4 -->
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Archivo Municipal</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2 active" href="<?php echo site_url("principal2/atractivos"); ?>">ver
+                                mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Auditoria Interna</h3>
+
+                        <div class="read_more">
+                            <a class="btn  btn-2b" href="<?php echo site_url("principal2/pueblos"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Antencion al ciudadano</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="<?php echo site_url("principal2/gastro"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Gerencia General</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="<?php echo site_url("principal2/trapiche"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+            <!-- end span_of_4 -->
+            <div class="span_of_4"><!-- start span_of_4 -->
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Gerencia de Cultura</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2 active" href="<?php echo site_url("principal2/atractivos"); ?>">ver
+                                mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Gerencia de Turismo</h3>
+
+                        <div class="read_more">
+                            <a class="btn  btn-2b" href="<?php echo site_url("principal2/pueblos"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Gerencia de servicios generales</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="<?php echo site_url("principal2/gastro"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Gerencia de Administración y Finanzas</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="<?php echo site_url("principal2/trapiche"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+            <!-- end span_of_4 -->
+            <div class="span_of_4"><!-- start span_of_4 -->
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>gerencia de desarrollo humano y educación</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2 active" href="<?php echo site_url("principal2/atractivos"); ?>">ver
+                                mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>gerencia de infraestructura y ordenamiento territorial</h3>
+
+                        <div class="read_more">
+                            <a class="btn  btn-2b" href="<?php echo site_url("principal2/pueblos"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>GERENCIA DE PLANIFICACIÓN Y PRESUPUESTO</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="<?php echo site_url("principal2/gastro"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Gerencia de Recursos Humanos</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="<?php echo site_url("principal2/trapiche"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+            <!-- end span_of_4 -->
+            <div class="span_of_4"><!-- start span_of_4 -->
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Comisión de Contrataciones Públicas</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2 active" href="<?php echo site_url("principal2/atractivos"); ?>">ver
+                                mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>concejo local de planificación pública</h3>
+
+                        <div class="read_more">
+                            <a class="btn  btn-2b" href="<?php echo site_url("principal2/pueblos"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>coordinación general de comunicación social protocolo e informatica</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="<?php echo site_url("principal2/gastro"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Coordinación General de Seguridad Ciudadana</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="<?php echo site_url("principal2/trapiche"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+            <!-- end span_of_4 -->
+            <!-- start span_of_4 -->
+            <div class="span_of_4"><!-- start span_of_4 -->
+
+            <div class="col-md-4 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Registros</h3>
+
+                        <div class="read_more">
+                            <a class="btn  btn-2b" href="<?php echo site_url("principal2/pueblos"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Secretaria ejecutiva</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="<?php echo site_url("principal2/gastro"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 span1_of_4">
+                    <div class="span4_of_list">
+                        <span><i class="fa fa-sitemap"></i></span>
+
+                        <h3>Sindicatura</h3>
+
+                        <div class="read_more">
+                            <a class="btn btn-2b" href="<?php echo site_url("principal2/trapiche"); ?>">ver mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <!-- end span_of_4 -->
+        </div>
+    </div>
+</div>
+<div class="main_bg"><!-- start main -->
+    <div class="container">
+        <div class="main_grid1">
+            <h3 class="style pull-left">Facilidades y Servicios</h3>
+
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
+<div class="main_btm1"><!-- start main_btm1 -->
+    <div class="container">
+        <div class="main about span_of_3">
+
+            <div class="col-md-4 span1_of_3">
+                <h4>Hoteles</h4>
+                <ul class="list-unstyled nav_list">
+                    <li><a href="#">Lorem ipsum dolor sit amet, consectetur</a></li>
+                    <li><a href="#">Nullam nec sapien eget metus</a></li>
+                    <li><a href="#">Curabitur pellentesque mauris tempor </a></li>
+                    <li><a href="#">Mauris dictum neque ornare nibh feugiat</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit amet, consectetur</a></li>
+                    <li><a href="#">Nullam nec sapien eget metus</a></li>
+                    <li><a href="#">Curabitur pellentesque mauris tempor </a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 span1_of_3">
+                <h4>Posadas</h4>
+                <ul class="list-unstyled nav_list">
+                    <li><a href="#">Lorem ipsum dolor sit amet, consectetur</a></li>
+                    <li><a href="#">Nullam nec sapien eget metus</a></li>
+                    <li><a href="#">Curabitur pellentesque mauris tempor </a></li>
+                    <li><a href="#">Mauris dictum neque ornare nibh feugiat</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit amet, consectetur</a></li>
+                    <li><a href="#">Nullam nec sapien eget metus</a></li>
+                    <li><a href="#">Curabitur pellentesque mauris tempor </a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 span1_of_3">
+                <h4>Restaurante</h4>
+                <ul class="list-unstyled nav_list">
+                    <li><a href="#">Lorem ipsum dolor sit amet, consectetur</a></li>
+                    <li><a href="#">Nullam nec sapien eget metus</a></li>
+                    <li><a href="#">Curabitur pellentesque mauris tempor </a></li>
+                    <li><a href="#">Mauris dictum neque ornare nibh feugiat</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit amet, consectetur</a></li>
+                    <li><a href="#">Nullam nec sapien eget metus</a></li>
+                    <li><a href="#">Curabitur pellentesque mauris tempor </a></li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
